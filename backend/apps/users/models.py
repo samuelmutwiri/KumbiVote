8f81468447f3b755ca39fe4be7591757ca9fa1dd
Eric Ndiku Musyoki <ericndiku@gmail.com>
2024-09-14 10:51:51 +0300

    Database flushed, migrations re-done

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,23 +1,22 @@
 import uuid
 
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("The Email field must be set")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        return self.create_user(email, password, **extra_fields)
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, email, password=None, **extra_fields):
+#         if not email:
+#             raise ValueError("The Email field must be set")
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, **extra_fields)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+#
+#     def create_superuser(self, email, password=None, **extra_fields):
+#         extra_fields.setdefault("is_staff", True)
+#         extra_fields.setdefault("is_superuser", True)
+#         return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser):
@@ -47,7 +46,7 @@ class User(AbstractBaseUser):
     updated_at = models.DateTimeField(auto_now=True)
     blockchain_id = models.CharField(max_length=100, blank=True)
 
-    objects = CustomUserManager()
+#    objects = CustomUserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
